c27ccc5de660651334bd83edafac282d63260e30
Meowterspace <meowterspace@gmail.com>
2018-09-30 11:53:27 -0700

    doda

---
+++ index.js
@@ -0,0 +1,12 @@
+let name = "start";
+console.log(name);
+
+// Cannot be a reserved keyword.. Let if else var..
+// Should be meaningful
+// Cannot start with a number
+// Cannot contain space or hyphen
+// Case sensitive 'nameNameName'
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
